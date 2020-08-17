4d4088223718cb06c7177e47fe3be70fea1f66d7
miroslav palka <palkamiroslav.v@gmail.com>
2020-08-17 15:38:41 +0100

    all models apart from users validated

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
-  has_many :messages, :reviews
+  has_many :messages
+  has_many :reviews
+
+  validates :title, :description, :booked_user_id, :start_time, :end_time, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :booking
   belongs_to :user
+
+  validates :content, :rating
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5] }
 end
