bb911b83e7361de91f273d65b997b22610409512
miroslav palka <palkamiroslav.v@gmail.com>
2020-08-18 11:29:41 +0100

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, except: :index do
-    resources :skills, only: [:index, :new, :edit, :create, :destroy, :update]
+  resources :users do
+    # resources :skills, only: [:index, :new, :edit, :create, :destroy, :update]
     # resources :reviews, only: [:index, :new, :edit, :create, :destroy, :update]
-    resources :bookings, only: [:index, :new, :create, :destroy, :show]
+    resources :bookings, only: [:new, :create]
     # resources :messages, only: [:index, :new, :edit, :create, :destroy, :update]  
   end
+  resources :bookings, only: [:index, :show, :destroy]
 end
