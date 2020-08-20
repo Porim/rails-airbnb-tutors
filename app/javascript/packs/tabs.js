b87e5606b2285c60596b4c6793803732597c3675
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-08-20 15:31:45 +0100

    changes to profile

--- app/javascript/packs/tabs.js
+++ app/javascript/packs/tabs.js
@@ -3,23 +3,32 @@ const editTabs = () => {
   const myProfile = document.querySelector('#my-profile');
   const myBookings = document.querySelector('#my-bookings');
   const cardProduct = document.querySelector('#profile-card-product');
+  const cardBookings = document.querySelector('#profile-bookings');
   myProfile.addEventListener('click', (event) => {
     event.preventDefault();
+    if (cardProduct.classList.value === 'card-product invisible') {
     myProfile.classList.toggle("active");
     myBookings.classList.toggle("active");
+    cardProduct.classList.toggle('invisible');
+    cardBookings.id = 'profile-bookings';
+  }
   })
 
   myBookings.addEventListener('click', (event) => {
     event.preventDefault();
+    if (myBookings.classList.value != 'active') {
     myProfile.classList.toggle("active");
     myBookings.classList.toggle("active");
-    cardProduct.classList.toggle('invisible')
+    cardProduct.classList.toggle('invisible');
+    cardBookings.classList.remove('invisible');
+    cardBookings.id = 'visible';
+    }
   })
   
-  editProfile.addEventListener('click', (event) => {
-    event.preventDefault();
-    myProfile.classList.toggle("active");
-    myBookings.classList.toggle("active");
-  })
+  // editProfile.addEventListener('click', (event) => {
+  //   event.preventDefault();
+  //   myProfile.classList.toggle("active");
+  //   myBookings.classList.toggle("active");
+  // })
 }
 export { editTabs }
